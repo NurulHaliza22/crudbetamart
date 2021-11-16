8a6c5766673262c89053a99902c4ebdec1a1bf16
NurulHaliza22 <Rienzt2@gmail.com>
2021-11-16 09:09:58 +0700

    first commit

---
+++ crudbetamart/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace crudbetamart
+{
+    partial class BetaMartCrud
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtJenis = new System.Windows.Forms.TextBox();
+            this.txtJumlah = new System.Windows.Forms.TextBox();
+            this.txtHarga = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.Label();
+            this.Nama = new System.Windows.Forms.Label();
+            this.Jenis = new System.Windows.Forms.Label();
+            this.Jumlah = new System.Windows.Forms.Label();
+            this.Harga = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(395, 41);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(178, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(395, 68);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(178, 20);
+            this.txtNama.TabIndex = 1;
+            // 
+            // txtJenis
+            // 
+            this.txtJenis.Location = new System.Drawing.Point(395, 94);
+            this.txtJenis.Name = "txtJenis";
+            this.txtJenis.Size = new System.Drawing.Size(178, 20);
+            this.txtJenis.TabIndex = 2;
+            // 
+            // txtJumlah
+            // 
+            this.txtJumlah.Location = new System.Drawing.Point(395, 120);
+            this.txtJumlah.Name = "txtJumlah";
+            this.txtJumlah.Size = new System.Drawing.Size(178, 20);
+            this.txtJumlah.TabIndex = 3;
+            // 
+            // txtHarga
+            // 
+            this.txtHarga.Location = new System.Drawing.Point(395, 147);
+            this.txtHarga.Name = "txtHarga";
+            this.txtHarga.Size = new System.Drawing.Size(178, 20);
+            this.txtHarga.TabIndex = 4;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(188, 206);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(537, 222);
+            this.dataGridView.TabIndex = 5;
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Location = new System.Drawing.Point(328, 44);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(18, 13);
+            this.ID.TabIndex = 6;
+            this.ID.Text = "ID";
+            // 
+            // Nama
+            // 
+            this.Nama.AutoSize = true;
+            this.Nama.Location = new System.Drawing.Point(328, 71);
+            this.Nama.Name = "Nama";
+            this.Nama.Size = new System.Drawing.Size(35, 13);
+            this.Nama.TabIndex = 7;
+            this.Nama.Text = "Nama";
+            // 
+            // Jenis
+            // 
+            this.Jenis.AutoSize = true;
+            this.Jenis.Location = new System.Drawing.Point(328, 97);
+            this.Jenis.Name = "Jenis";
+            this.Jenis.Size = new System.Drawing.Size(31, 13);
+            this.Jenis.TabIndex = 8;
+            this.Jenis.Text = "Jenis";
+            // 
+            // Jumlah
+            // 
+            this.Jumlah.AutoSize = true;
+            this.Jumlah.Location = new System.Drawing.Point(328, 123);
+            this.Jumlah.Name = "Jumlah";
+            this.Jumlah.Size = new System.Drawing.Size(40, 13);
+            this.Jumlah.TabIndex = 9;
+            this.Jumlah.Text = "Jumlah";
+            // 
+            // Harga
+            // 
+            this.Harga.AutoSize = true;
+            this.Harga.Location = new System.Drawing.Point(328, 150);
+            this.Harga.Name = "Harga";
+            this.Harga.Size = new System.Drawing.Size(36, 13);
+            this.Harga.TabIndex = 10;
+            this.Harga.Text = "Harga";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::crudbetamart.Properties.Resources.betamart1;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 145);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(25, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 29);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "BETAMART\r\n";
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(55, 235);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(75, 23);
+            this.btnView.TabIndex = 13;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(55, 285);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(55, 337);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(55, 386);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel1.Location = new System.Drawing.Point(244, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(415, 174);
+            this.panel1.TabIndex = 17;
+            // 
+            // BetaMartCrud
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Harga);
+            this.Controls.Add(this.Jumlah);
+            this.Controls.Add(this.Jenis);
+            this.Controls.Add(this.Nama);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.txtHarga);
+            this.Controls.Add(this.txtJumlah);
+            this.Controls.Add(this.txtJenis);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BetaMartCrud";
+            this.Text = "BetaMartCrud";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtJenis;
+        private System.Windows.Forms.TextBox txtJumlah;
+        private System.Windows.Forms.TextBox txtHarga;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label Nama;
+        private System.Windows.Forms.Label Jenis;
+        private System.Windows.Forms.Label Jumlah;
+        private System.Windows.Forms.Label Harga;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ crudbetamart/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace crudbetamart
+{
+    public partial class BetaMartCrud : Form
+    {
+
+        private OleDbConnection con = new OleDbConnection();
+
+        public BetaMartCrud()
+        {
+            InitializeComponent();
+            con.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\HP\Documents\bmcrud.accdb";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (txtNama.Text == "" || txtJenis.Text == "" || txtJumlah.Text == "" || txtHarga.Text == "")
+                {
+                    MessageBox.Show("REQUIRED MISSING FIELD!", "MESSAGE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                con.Open();
+                OleDbCommand cmd = new OleDbCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "insert into bmdb (ID,Nama,Jenis,Jumlah,Harga)values('" + txtId.Text + "','" + txtNama.Text + "','" + txtJenis.Text + "','" + txtJumlah.Text + "','" + txtHarga.Text + "')";
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("NEW DATA HAS BEEN SUCCESSFULLY SAVED.", "MASSAGE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+
+        }
+
+        void ClearText()
+        {
+            
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                con.Open();
+                OleDbCommand cmd = new OleDbCommand();
+                cmd.Connection = con;
+                string query = "select * from bmdb";
+
+                cmd.CommandText = query;
+
+
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView.DataSource = dt;
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (txtNama.Text == "" || txtJenis.Text == "" || txtJumlah.Text == "" || txtHarga.Text == "")
+                {
+                    MessageBox.Show("REQUIRED MISSING FIELD!", "MESSAGE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                if (MessageBox.Show("WANT TO UPDATE THIS RECORD?", "MESSAGE", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    con.Open();
+                    OleDbCommand cmd = con.CreateCommand();
+                    cmd.Connection = con;
+                    string query = "update bmdb set Nama='" + txtNama.Text + "' ,Jenis='" + txtJenis.Text + "' ,Jumlah='" + txtJumlah.Text + "' ,Harga='" + txtHarga.Text + "' WHERE ID='" + txtId.Text + "'";
+                    cmd.CommandText = query;
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("NEW DATA HAS BEEN SUCCESSFULLY UPDATED.", "MASSAGE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("WANT TO DELETE THIS RECORD?", "MESSAGE", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+
+                    con.Open();
+                    OleDbCommand cmd = con.CreateCommand();
+                    cmd.Connection = con;
+                    cmd.CommandText = "delete from bmdb Where ID='" + txtId.Text + "'";
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("NEW DATA HAS BEEN SUCCESSFULLY DELETED.", "MASSAGE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+    }
+}
